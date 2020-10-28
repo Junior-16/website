ab3d72727179516a42897b556dac8faf96b7dab6
Junior V. Ramisch <junior.ramisch@gmail.com>
2020-10-28 19:52:58 -0300

    chore: uses pandas and object oriented programming

--- src/topic-modeling/titanic/__init__.py
+++ src/topic-modeling/titanic/__init__.py
@@ -1,4 +1,4 @@
 from setup import *
 
 if __name__ == "__main__":
-    svm_model()
+    svm_model = SVMModel()
--- src/topic-modeling/titanic/setup.py
+++ src/topic-modeling/titanic/setup.py
@@ -1,4 +1,23 @@
 from sklearn import svm 
+import pandas as pd
 
-def svm_model():
-    print(svm.__doc__)
+DATASET_PATH = "../../../datasets/titanic/"
+
+class Dataset(object):
+    def __init__(self):
+        self.training_set = pd.read_csv(DATASET_PATH + "train.csv", sep=",", header=None)
+        self.__select_features()
+        
+    def __select_features(self):
+        print("\tSelecting features...")
+        print(dir(self.training_set))
+
+class SVMModel(Dataset):
+    def __init__(self):
+        super().__init__()
+        
+    def train(self):
+        pass
+
+    def test():
+        print(svm.__doc__)
