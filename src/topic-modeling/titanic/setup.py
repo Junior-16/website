1d2b85b4e564e32db30d18be940095f940f54587
Junior V. Ramisch <junior.ramisch@gmail.com>
2020-10-28 20:22:46 -0300

    chore: implements feature selection
    selected features: Survived, Pclass, Sex, Age, Fare

--- src/topic-modeling/titanic/setup.py
+++ src/topic-modeling/titanic/setup.py
@@ -4,13 +4,21 @@ import pandas as pd
 DATASET_PATH = "../../../datasets/titanic/"
 
 class Dataset(object):
+
     def __init__(self):
-        self.training_set = pd.read_csv(DATASET_PATH + "train.csv", sep=",", header=None)
-        self.__select_features()
         
+        self.dataset_slice_1 = {
+            "selected_feat" : "Survived, Pclass, Sex, Age, Fare",
+            "dropped_feat"  : ["PassengerId", "Name", "SibSp", "Parch", "Ticket", "Cabin", "Embarked"]
+        }
+        
+        self.training_set = pd.read_csv(DATASET_PATH + "train.csv", sep=",")
+        self.__select_features()
+    
     def __select_features(self):
-        print("\tSelecting features...")
-        print(dir(self.training_set))
+        print(self.dataset_slice_1["dropped_feat"])
+        self.training_set = self.training_set.drop(self.dataset_slice_1["dropped_feat"], axis=1)
+        print(self.training_set)
 
 class SVMModel(Dataset):
     def __init__(self):
